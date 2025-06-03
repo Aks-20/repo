81757db9dfab0381873d45f133d96dfb2d5358fc
Aks-20 <akshatgupta0043@gmail.com>
2025-06-03 12:13:26 +0530

    commit

--- portfolioApp/src/App.jsx
+++ portfolioApp/src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './Pages/Contact.jsx';
 const App = () => {
   return (
     <Router>
-      {/* Wrap everything in a div with dark mode class handling */}
+    
       <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
         <NavBar />
         <div className="content">
